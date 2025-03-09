8bed50666ad11f99481058a9b300a4469bbafab9
kchan5071 <kchan5071@sdsu.edu>
2025-03-08 22:57:35 -0800

    added changable python executable

---
+++ README.md
@@ -0,0 +1,17 @@
+# Python Service Creator
+
+this project starts and stops python services based on what is in the services folder, the current implementation supports unix-socket termination in /usr/test
+
+the list of services started and their process ID's are stored in services.csv
+
+## Usage
+
+to start:
+```Bash
+./start_services.py
+```
+
+to stop:
+```
+./stop_services.py
+```
--- start_services.py
+++ start_services.py
@@ -7,8 +7,6 @@ import argparse
 def get_service_directory():
     return os.path.join(os.path.dirname(__file__), 'services')
 
-def get_python_executable():
-    return '/opt/homebrew/anaconda3/bin/python'
 
 def get_process_name_list(user_directory=None):
     if user_directory is None:
@@ -21,28 +19,24 @@ def get_process_name_list(user_directory=None):
 
     return process_name_list
 
-def start_service(service_name, service_directory):
-    if service_directory is None:
-        print("No service directory provided.")
-        quit()
-
+def start_service(service_name, service_directory, python_executable):
     for file in os.listdir(service_directory):
         if file.endswith('.py') and file.startswith(service_name):
-
-            process = subprocess.Popen([get_python_executable(), os.path.join(service_directory, service_name)], cwd=service_directory)
+            process = subprocess.Popen([python_executable, os.path.join(service_directory, service_name)], cwd=service_directory)
             pid = process.pid
             return (service_name, pid)
         
     return (None, None)
 
-def start_services(user_directory):
+def start_services(user_directory, python_executable):
     if user_directory is None:
-        print("No service directory provided.")
+        print("No service directory provided, using default: cwd/services")
     directory = get_service_directory()
     name_list = get_process_name_list(directory)
     service_list = []
     for name in name_list:
-        service_list.append(start_service(name, directory))
+        service_list.append(start_service(name, directory, python_executable))
+        print(f'Started service {name}.')
     return service_list
 
 def write_to_csv(services, filename='services.csv'):
@@ -50,12 +44,11 @@ def write_to_csv(services, filename='services.csv'):
         for service in services:
             file.write(f'{service[0]},{service[1]}\n')
 
-
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Start services.' )
-    parser.add_argument('-d','--service_directory', help='The directory containing the services.', default=None)
+    parser.add_argument('-d', '--service_directory', help='The directory containing the services.', default=None)
+    parser.add_argument('-p', '--python_executable', help='The Python executable to use.', default='/opt/homebrew/anaconda3/bin/python')
     args = parser.parse_args()
-    print(args.service_directory)
-    service_list = start_services(args.service_directory)
+    service_list = start_services(args.service_directory, args.python_executable)
     write_to_csv(service_list)
     print('Services started.')
