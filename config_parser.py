ad3b762fcb1b78cff2d967184321b3f080b80163
kchan5071 <kchan5071@sdsu.edu>
2025-04-13 16:00:12 -0700

    added typing annotations

--- config_parser.py
+++ config_parser.py
@@ -1,6 +1,6 @@
 import yaml
 
-def read_config(config_file):
+def read_config(config_file: str) -> dict:
     with open(config_file, 'r') as file:
         config = yaml.safe_load(file)
     return config
